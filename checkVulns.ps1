96141f72e2e1f5c11ce650e9f39b33fe677f57b0
syoungs <syoungs@scottlogic.com>
2023-03-07 17:23:44 +0000

    added comments to explain script

--- checkVulns.ps1
+++ checkVulns.ps1
@@ -1,10 +1,27 @@
 . ./variable.ps1
 
+########################
+#    
+#   Check dependencies from a project in Dependency track.
+#
+#   Change $url to Dependency track root URL. 
+#   Change $uuid to the project unique identifier.
+#
+#   This file uses another file called variable.ps1 as a secrets file,
+#   it contains the API token used to access the Dep track API.
+#   This can be manually added into the file under a $token variable. 
+#   Or a new file called variable.ps1 can be created in this directory
+#   with the code $token = <token>
+#
+#########################
+
 [int]$awsCheck = 1
 $packageArray = @()
+$url = "http://localhost:8081"
+$uuid = "04444305-bac9-4a07-8b12-b83aa4e4523e"
 
 $Params = @{
-    "URI"       = 'http://localhost:8081/api/v1/component/project/04444305-bac9-4a07-8b12-b83aa4e4523e'
+    "URI"       = "$url/api/v1/component/project/$uuid"
     "Method"    = 'GET'
     "Headers"   = @{
         "accept"    = "application/json"
